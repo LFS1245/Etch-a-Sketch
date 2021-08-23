b3389cf7abeeb1404649c7eba4e36ef8cc19f973
LFS1245 <luiz.f.souza@live.com>
2021-08-23 20:26:39 -0300

    basic app working

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const container = document.querySelector(".sketch-container");
+const gridRange = document.querySelector(".grid-size");
 
+//make grid
 for (let i = 0; i < 256; i++) {
-  container.appendChild(document.createElement("div"));
+  var pixel = container.appendChild(document.createElement("div"));
+  pixel.classList.add("pixel");
 }
+
+//draw
+const gridElement = document.querySelectorAll(".pixel");
+gridElement.forEach((element) =>
+  element.addEventListener("mouseenter", (e) => element.classList.add("active"))
+);
