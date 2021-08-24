5d49621237057057fc9eda2a1787df4bbb79b6ac
LFS1245 <luiz.f.souza@live.com>
2021-08-24 17:44:26 -0300

    add resize function

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 const container = document.querySelector(".sketch-container");
 const gridRange = document.querySelector(".grid-size");
 
-//make grid
-for (let i = 0; i < 256; i++) {
-  var pixel = container.appendChild(document.createElement("div"));
-  pixel.classList.add("pixel");
-}
+function drawGrid(value = 16) {
+  if (container.childElementCount) {
+    while (container.childElementCount > 0) {
+      container.removeChild(container.lastChild);
+    }
+  }
+  console.log(gridRange.value);
+  console.log(value);
+  container.style.setProperty("grid-template-columns", `repeat(${value}, 1fr)`);
+  for (let i = 0; i < value * value; i++) {
+    let pixel = container.appendChild(document.createElement("div"));
+    pixel.classList.add("pixel");
+  }
 
-//draw
-const gridElement = document.querySelectorAll(".pixel");
-gridElement.forEach((element) =>
-  element.addEventListener("mouseenter", (e) => element.classList.add("active"))
-);
+  // Draw inside grid
+  const gridElement = document.querySelectorAll(".pixel");
+  gridElement.forEach((element) =>
+    element.addEventListener("mouseenter", (e) =>
+      element.classList.add("active")
+    )
+  );
+}
+drawGrid();
+gridRange.addEventListener("change", (e) => {
+  const value = gridRange.value;
+  drawGrid(value);
+});
