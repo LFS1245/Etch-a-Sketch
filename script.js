1b55c07af16bbee53e4a44da21d1ef01a03ee4fd
LFS1245 <luiz.f.souza@live.com>
2021-08-25 13:13:31 -0300

    finished script, need to come back later when learn how to do some things more efficiently

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const container = document.querySelector(".sketch-container");
 const gridRange = document.querySelector(".grid-size");
-const blackButton = document.querySelector("#color-mode");
+const grayButton = document.querySelector("#color-mode");
 const eraserButton = document.querySelector("#eraser-mode");
 const rainbowButton = document.querySelector("#rainbow-mode");
+const clearButtons = document.querySelector("#clear");
 
 let pixel;
 function drawGrid(value = 16) {
+  // draw and resize grid as needed
   if (container.childElementCount) {
-    //clear board
-    while (container.childElementCount > 0) {
-      container.removeChild(container.lastChild);
-    }
+    container.innerHTML = "";
   }
   container.style.setProperty("grid-template-columns", `repeat(${value}, 1fr)`); //resize
   for (let i = 0; i < value * value; i++) {
@@ -24,22 +23,73 @@ gridRange.addEventListener("change", (e) => {
   drawGrid(value);
 });
 
-function toggleButton(button) {
-  button.classList.toggle("active");
+function clearGrid() {
+  container.childNodes.forEach((block) =>
+    block.style.setProperty("background-color", "#fff")
+  );
 }
 
-function activateGray() {
-  // Draw inside grid
-  const gridElement = document.querySelectorAll(".pixel");
-  if (blackButton.classList)
-    gridElement.forEach((element) =>
-      element.addEventListener("mouseenter", (e) =>
-        // element.classList.add("active")
-        element.style.setProperty("background-color", "#333")
-      )
-    );
+//activate the buttons and color mode
+function toggleButton(button) {
+  if (grayButton.classList.contains("activated")) {
+    grayButton.classList.remove("activated");
+  }
+  if (eraserButton.classList.contains("activated")) {
+    eraserButton.classList.remove("activated");
+  }
+  if (rainbowButton.classList.contains("activated")) {
+    rainbowButton.classList.remove("activated");
+  }
+
+  if (button === grayButton) {
+    button.classList.add("activated");
+    activateGrayMode();
+  } else if (button === eraserButton) {
+    button.classList.add("activated");
+    activateEraserMode();
+  } else if (button === rainbowButton) {
+    button.classList.add("activated");
+    activateColorMode();
+  }
 }
-blackButton.addEventListener("click", (e) => {
-  toggleButton(e.target);
-  activateGray();
-});
+
+function activateGrayMode() {
+  // Draw inside grid with gray ink
+  const gridElement = document.querySelectorAll(".pixel");
+  gridElement.forEach((element) =>
+    element.addEventListener("mouseenter", (e) =>
+      element.style.setProperty("background-color", "#333")
+    )
+  );
+}
+
+function activateEraserMode() {
+  const gridElement = document.querySelectorAll(".pixel");
+  gridElement.forEach((element) =>
+    element.addEventListener("mouseenter", (e) =>
+      element.style.setProperty("background-color", "#fff")
+    )
+  );
+}
+
+function activateColorMode() {
+  const gridElement = document.querySelectorAll(".pixel");
+  gridElement.forEach((element) =>
+    element.addEventListener("mouseenter", (e) =>
+      element.style.setProperty("background-color", `${randomRGB()}`)
+    )
+  );
+}
+
+function randomRGB() {
+  const randomR = Math.random() * 255;
+  const randomG = Math.random() * 255;
+  const randomB = Math.random() * 255;
+  const randomColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+  return randomColor;
+}
+
+grayButton.addEventListener("click", toggleButton.bind(this, grayButton));
+eraserButton.addEventListener("click", toggleButton.bind(this, eraserButton));
+rainbowButton.addEventListener("click", toggleButton.bind(this, rainbowButton));
+clearButtons.addEventListener("click", () => clearGrid());
